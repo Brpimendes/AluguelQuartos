b4459e9b9cbff5972380cf84bc17b694e0567a5b
Bruno Mendes <brpimendes@gmail.com>
2019-07-23 00:30:15 -0300

    Projeto que simula o aluguel de quartos em uma pousada. com o cadastro de nome e email.

---
+++ AluguelDeQuartos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace AluguelDeQuartos {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("How many rooms will be rented: ");
+            int numeroDeQuartos = int.Parse(Console.ReadLine());
+
+            Quarto[] quartos = new Quarto[10];
+
+            for (int i = 1; i <= numeroDeQuartos; i++) {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i}: ");
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Numero do quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                quartos[quarto] = new Quarto(name, email);
+            }
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Relatório dos Quartos alugados:");
+            for (int i = 0; i < 10; i++) {
+                if (quartos[i] != null) {
+                    Console.WriteLine(i +": "+ quartos[i]);
+                }                
+            }
+        }
+    }
+}
---
+++ AluguelDeQuartos/Quarto.cs
@@ -0,0 +1,20 @@
+﻿
+namespace AluguelDeQuartos {
+    class Quarto {
+        public string Nome { get; set; }
+        public string Email { get; set; }        
+
+        public Quarto() {
+        }
+
+        public Quarto(string name, string email) {
+            Nome = name;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return "Nome: " + Nome
+                   + ", e-mail: " + Email;
+        }
+    }
+}
